cbfcc1500f52ecd5ceec0b10f31d0fe705b4b7a6
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 19:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(musician, instrument){
+  var results = [];
+  for(var i=0; i<musician.length; i++){
+    results[i] = (`${musician[i]} plays ${instrument[i]}`)
+  }
+  return results;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  while(i < facts.length){
+    facts[i]=`${facts[i]}!!!`
+    i++
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num){
+var results = [];
+do{
+  var i = 0
+  results[i]= 'I love the Beatles!'
+  i++
+}while(num<15)
+
+return results;
+  
+}
